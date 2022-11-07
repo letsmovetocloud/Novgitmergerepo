fe7f8076d49c025105cc787f96a6906b6166092f
letsmovetocloud <88219893+letsmovetocloud@users.noreply.github.com>
2022-11-07 17:57:34 +0530

    ec2serverdetils &cidr block ranges are changed
    ec2serverdetils &cidr block ranges are changed

--- ec2.tf
+++ ec2.tf
@@ -17,7 +17,7 @@ provider "aws" {
 #Resource Block
 resource "aws_instance" "ec2testserver" {
   ami           = "ami-041db4a969fe3eb68" 
-  instance_type = "t2.micro"
+  instance_type = "d2.t3.medium"
 
 }
 
@@ -29,9 +29,9 @@ module "vpc" {
 
   # VPC Basic Details
   name = "vpc-dev"
-  cidr = "10.0.0.0/16"   
+  cidr = "192.168.0.0/16"   
   azs                 = ["us-east-1a"]
-  private_subnets     = ["10.0.1.0/24"]
-  public_subnets      = ["10.0.101.0/24"]
+  private_subnets     = ["192.168.1.0/24"]
+  public_subnets      = ["192.168.101.0/24"]
 
 #new comment added 
